ee297ae878a55b6e1f5f00dcd96c8deb17380326
Krs Joseph <krspjoseph@gmail.com>
2015-10-29 02:17:38 +0000

    Add setup to get token

---
+++ get_token.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+
+
+uri = 'http://challenge.code2040.org/api/register'
+email = 'krspjoseph@gmail.com'
+github = 'https://github.com/krsjoseph/code2040'
+token = 'WEsjMk7cIh'
+
+
+
+response =  HTTParty.post(uri, 
+  :body => {:email => "#{email}", :github => "#{github}"}.to_json,
+  :headers => {'Content-Type' => 'application/json'} )
+
+puts response.body
