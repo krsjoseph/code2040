28d5e4ee14edf92594df3ab603190dacdf86cf01
Krs Joseph <krspjoseph@gmail.com>
2015-10-29 15:15:42 -0400

    remove extra newlines

--- get_token.rb
+++ get_token.rb
@@ -1,12 +1,9 @@
 require 'httparty'
 
-
 uri = 'http://challenge.code2040.org/api/register'
 email = 'krspjoseph@gmail.com'
 github = 'https://github.com/krsjoseph/code2040'
 token = 'WEsjMk7cIh'
-
-
 
 response =  HTTParty.post(uri, 
   :body => {:email => "#{email}", :github => "#{github}"}.to_json,
